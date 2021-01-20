3945b0e8d5c5379579411d6c04bb464362d13afe
Megan Brockavich <brockavichmegan@gmail.com>
2021-01-20 08:35:43 -0600

    Feat: Create/remove active class on scroll

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@
 // Global Variables
 const nav_bar = document.getElementById("navbar__list");
 const nav_links = ["Top","Section 1", "Section 2", "Section 3", "Section 4"];
+const sections = document.querySelectorAll("section")
 
 // build the nav
 nav_links.forEach((nav_link, b) => {
@@ -29,16 +30,26 @@ nav_links.forEach((nav_link, b) => {
 });
 
 // Add class 'active' to section when near top of viewport
+function activeClass(){
+//  console.log(sections);
+  sections.forEach((sections, c) => {
+    let section = document.querySelector(`#section${c+0}`);
+    let bounding = section.getBoundingClientRect();
+//    console.log(sections, bounding.top, bounding.bottom);
+    if (bounding.top < 1 && bounding.bottom > 1){
+      section.classList.add('your-active-class');
+    }
+    else {
+      if (section.classList.contains('your-active-class')){
+        section.classList.remove('your-active-class')
+      }
+    }
+  })
+};
 
-// What's being viewed
-const section = document.querySelector('.landing__container');
-const page = section.getBoundingClientRect();
+document.addEventListener("scroll", activeClass)
+// try this next: https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
 
-document.addEventListener('scroll', function (){
-  const isInViewport = page.top <= 0 &&
-          page.top <= 0 ;
-  console.log(isInViewport);
-});
 
 /**
  * End Main Functions
